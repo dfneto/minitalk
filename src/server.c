40bbacd38a28134611d04c0e088c2b6b4aadb395
davifern <pdavidfneto@gmail.com>
2023-08-24 15:46:16 +0200

    moved the 2 functions to just 1

--- src/server.c
+++ src/server.c
@@ -16,51 +16,22 @@ int c = 0;
 int i = 0;
 int len;
 
-void    func_()
-{
-    ft_printf("Waka waka 1");
-}
-
-void    func2_()
-{
-    ft_printf("Waka waka 2");
-}
-
 void    func(int signum)
 {   
-    c = c | 1;
     counter_temp++;
+    if (signum == 30) //1
+        c = c | 1;
+    
     if (counter_temp == 32)
     {
         len = c;
         ft_printf("%d", len);
         ft_printf("\n");
         counter_temp = 0;
-        signal(SIGUSR1, SIG_DFL);
-        signal(SIGUSR2, SIG_DFL);
-        signal(SIGUSR1, func_);
-        signal(SIGUSR2, func2_);
     }
+
     c = c << 1; //TODO: se eu mover mask ao invés do char. Neste caso teria que ser 128?
 }
-
-void    func2(int signum)
-{
-    counter_temp++;
-    if (counter_temp == 32)
-    {
-        len = c;
-        ft_printf("%d", len);
-        ft_printf("\n");
-        counter_temp = 0;
-        signal(SIGUSR1, SIG_DFL);
-        signal(SIGUSR2, SIG_DFL);
-        signal(SIGUSR1, func_);
-        signal(SIGUSR2, func2_);
-    }
-    c = c << 1;
-}
-
 
 int main()
 {
@@ -69,7 +40,7 @@ int main()
     pid = getpid();
     ft_printf("My pid: %d\n", pid);
     signal(SIGUSR1, func);
-    signal(SIGUSR2, func2);
+    signal(SIGUSR2, func);
     while(1)
     {
         pause();   //TODO: colocar as declaracoes de signal aqui e testar o consumo de memoria sem o pause
